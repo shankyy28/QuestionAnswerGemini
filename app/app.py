849c05579891e02fd8eadbf702c8b2b69afa4b90
Shashank Dimri <shashankdimri28@gmail.com>
2024-10-22 01:31:30 +0530

    Fixed gitignore

--- app/app.py
+++ app/app.py
@@ -1,25 +1,29 @@
 import os
-from flask import Flask
+from flask import Flask, request, jsonify
 import google.generativeai as genai
-from dotenv import load_dotenv, get_key
+from dotenv import load_dotenv
 
 load_dotenv()
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods = ['POST', 'GET'])
 
 def index():
 
-    gemini_key = os.getenv("GOOGLE_API_KEY")
+    try:
+        gemini_key = os.getenv("GOOGLE_API_KEY")
+        genai.configure(api_key = gemini_key)
+        model = genai.GenerativeModel('gemini-1.5-flash')
 
-    genai.configure(api_key = gemini_key)
+        data = request.get_json()
+        question = data.get("question")
+        response = model.generate_content(question)
 
-    model = genai.GenerativeModel('gemini-1.5-flash')
-
-    response = model.generate_content("Who is Jeff Bezos?")
-
-    return (response.text)
+        return (response.text), 200
+    
+    except:
+        return jsonify({"error" : "Internal error"}), 500
 
 if __name__ == "__main__":
     app.run(debug = True)
